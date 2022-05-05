d1038ffa036c2741339f3b7186e4964ab0bb1c31
code-carpenter <funsyte@yahoo.com>
2022-05-05 18:41:23 -0400

    Update vpc-refference.tf

--- vpc-refference.tf
+++ vpc-refference.tf
@@ -1,127 +1,122 @@
 # Create VPC
 # terraform aws create vpc
 resource "aws_vpc" "vpc" {
-  cidr_block              = 
+  cidr_block              = "10.0.0.0/16"
  
 
   tags      = {
-    Name    = 
+    Name    = "oloye_vpc"
   }
 }
 
 # Create Internet Gateway and Attach it to VPC
 # terraform aws create internet gateway
 resource "aws_internet_gateway" "internet-gateway" {
-  vpc_id    =
+  vpc_id    = aws_vpc.vpc.id
 
   tags      = {
-    Name    = 
+    Name    = "oloye_vpc_igw"
   }
 }
 
 # Create Public Subnet 1
 # terraform aws create subnet
 resource "aws_subnet" "public-subnet-1" {
-  vpc_id                  = 
-  cidr_block              = 
-  availability_zone       = 
-  map_public_ip_on_launch = 
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-2a"
+  map_public_ip_on_launch = true
 
   tags      = {
-    Name    = 
+    Name    = "oloye_vpc_public_subnet_1"
   }
 }
 
 # Create Public Subnet 2
 # terraform aws create subnet
 resource "aws_subnet" "public-subnet-2" {
-  vpc_id                  = 
-  cidr_block              = 
-  availability_zone       = 
-  map_public_ip_on_launch = 
-
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-2b"
+  map_public_ip_on_launch = true
   tags      = {
-    Name    = 
+    Name    = "oloye_vpc_public_subnet_2"
   }
 }
 
 # Create Route Table and Add Public Route
 # terraform aws create route table
 resource "aws_route_table" "public-route-table" {
-  vpc_id       = 
+  vpc_id       = aws_vpc.vpc.id
 
   route {
-    cidr_block =
-    gateway_id = 
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet-gateway.id
   }
 
   tags       = {
-    Name     =
+    Name     = "oloye_public_rt"
   }
 }
 
 # Associate Public Subnet 1 to "Public Route Table"
 # terraform aws associate subnet with route table
 resource "aws_route_table_association" "public-subnet-1-route-table-association" {
-  subnet_id           = 
-  route_table_id      =
+  subnet_id           = aws_subnet.public-subnet-1.id
+  route_table_id      = aws_route_table.public-route-table.id
 }
 
 # Associate Public Subnet 2 to "Public Route Table"
 # terraform aws associate subnet with route table
 resource "aws_route_table_association" "public-subnet-2-route-table-association" {
-  subnet_id           = 
-  route_table_id      = 
+  subnet_id           = aws_subnet.public-subnet-2.id
+  route_table_id      = aws_route_table.public-route-table.id
 }
 
 # Create Private Subnet 1
 # terraform aws create subnet
 resource "aws_subnet" "private-subnet-1" {
-  vpc_id                   = 
-  cidr_block               = 
-  availability_zone        = 
-  map_public_ip_on_launch  = 
+  vpc_id                   = aws_vpc.vpc.id
+  cidr_block               = "10.0.3.0/24"
+  availability_zone        = "us-east-2c"
 
   tags      = {
-    Name    =
+    Name    = "oloye_vpc_private_subnet_1"
   }
 }
 
 # Create Private Subnet 2
 # terraform aws create subnet
 resource "aws_subnet" "private-subnet-2" {
-  vpc_id                   = 
-  cidr_block               = 
-  availability_zone        = 
-  map_public_ip_on_launch  =
+  vpc_id                   = aws_vpc.vpc.id
+  cidr_block               = "10.0.4.0/24"
+  availability_zone        = "us-east-2a"
 
   tags      = {
-    Name    =
+    Name    = "oloye_vpc_private_subnet_2"
   }
 }
 
 # Create Private Subnet 3
 # terraform aws create subnet
 resource "aws_subnet" "private-subnet-3" {
-  vpc_id                   = 
-  cidr_block               = 
-  availability_zone        = 
-  map_public_ip_on_launch  =
+  vpc_id                   = aws_vpc.vpc.id
+  cidr_block               = "10.0.5.0/24"
+  availability_zone        = "us-east-2b"
 
   tags      = {
-    Name    = 
+    Name    = "oloye_vpc_private_subnet_3"
   }
 }
 
 # Create Private Subnet 4
 # terraform aws create subnet
 resource "aws_subnet" "private-subnet-4" {
-  vpc_id                   = 
-  cidr_block               = 
-  availability_zone        =
-  map_public_ip_on_launch  =
+  vpc_id                   = aws_vpc.vpc.id
+  cidr_block               = "10.0.6.0/24"
+  availability_zone        = "us-east-2c"
 
   tags      = {
-    Name    =
+    Name    = "oloye_vpc_private_subnet_4"
   }
 }
